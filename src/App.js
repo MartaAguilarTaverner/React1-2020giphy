00e8470b0b6f030dddb391b82e1ed00ebbc5dd47
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2023-07-17 21:10:17 +0200

    components and services

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ListOfGifs from './components/ListOfGifs';
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vamos a editar esto
-        </p>
-      </header>
+      <section className="App-content">
+        <ListOfGifs keyword='mapache' />
+      </section>
     </div>
   );
 }
 
-export default App;
+/*
+FOREACH GO OVER A LIST OF ELEMENTS BUT DON'T SHOW THEM BUT THE MAP RETURN THE RESULTS IN THE SCREEN.
+IN THE ARROW FUNCTION RETURNS IF IS ONLY ONE LINE
+THE USE STATE THE STATE CHANGES AND RENDER THE COMPONENT EACH TIME IT CHANGES, AND
+FOR EACH CHANGE IT ACTIVATES THE USE EFFECT
+AND THE FUNCTION OF IT IS ACTIVATED EACH TIME OUR COMPONENT RENDER(STATE CHANGES)
+ */
